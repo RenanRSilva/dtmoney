ad2baa9d528c4581c5c13cb77bce0f45016dc4a5
--global <--global>
2022-01-08 10:24:55 -0300

    add miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs'
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          transaction: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          ceatedAt: new Date(),
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
