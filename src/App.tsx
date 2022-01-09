74835fb7dfc46baaf0631d1f7580802fcf89ec95
RenanRSilva <renanrambuls@gmail.com>
2022-01-09 18:55:51 -0300

    styling modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import Modal from 'react-modal'
 import { GlobalStyle } from "./styles/global";
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
 import { useState } from 'react';
-
-Modal.setAppElement('root');
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
@@ -20,9 +18,7 @@ export function App() {
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
-      <Modal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal} >
-          <h2>Cadastrar Transação</h2>
-        </Modal>
+      <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal} />
       <GlobalStyle />
     </>
   );
---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,43 @@
+import Modal from 'react-modal'
+import closeImg from '../../assets/close.svg';
+import { Container } from './styles'
+
+interface NewTransactionModalProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
+}
+
+export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName='react-modal-overlay'
+      className='react-modal-content'
+    >
+      <button
+        type='button'
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar Modal" />
+      </button>
+      <Container>
+        <h2>Cadastrar Transação</h2>
+        <input placeholder="Titulo" />
+        <input
+          type="number"
+          placeholder="Valor"
+        />
+        <input
+          type="number"
+          placeholder="Categoria"
+        />
+        <button type="submit">
+          Cadastrar
+        </button>
+      </Container>
+    </Modal>
+  )
+}
