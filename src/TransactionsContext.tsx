5fa1f2f61a6bdc414889a04f8bbf801988f48e35
RenanRSilva <renanrambuls@gmail.com>
2022-01-28 15:02:13 -0300

    feat: add new transactions context and render

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -7,7 +7,7 @@ interface Transaction {
   amount: number;
   type: string;
   category: string;
-  createdAt: string;
+  createdAt: Date;
 }
 
 type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>;
@@ -20,7 +20,7 @@ interface TransactionsProviderProps {
 
 interface TransactionsContextData {
   transactions: Transaction[];
-  createTransaction: (transaction: TransactionInput) => void
+  createTransaction: (transaction: TransactionInput) => Promise<void>
 }
 
 export const TransactionsContext = createContext<TransactionsContextData>(
@@ -35,9 +35,17 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
       .then(response => setTransactions(response.data.transactions))
   }, []);
 
-  function createTransaction(transaction: TransactionInput) {
+  async function createTransaction(transactionInput: TransactionInput) {
+    const response = await api.post('/transactions', {
+      ...transactionInput,
+      createdAt: new Date(),
+    })
+    const { transaction } = response.data;
 
-    api.post('/transactions', transaction)
+    setTransactions([
+      ...transactions,
+      transaction,
+    ]);
   }
 
   return (
--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -23,11 +23,16 @@ export function TransactionTable() {
             <tr key={transaction.id}>
               <td>{transaction.title}</td>
               <td className={transaction.type}>
-                {transaction.amount}
+                {new Intl.NumberFormat('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL'
+                }).format(transaction.amount)}
               </td>
               <td>{transaction.category}</td>
               <td>
-                {transaction.createdAt}
+                {new Intl.DateTimeFormat('pt-BR').format(
+                  new Date(transaction.createdAt)
+                )}
               </td>
             </tr>
           ))}
